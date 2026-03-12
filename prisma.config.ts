ee5084f94f5b605ea858291c796d00cd1411a74f
陈志文 <zhiwenichen@gmail.com>
2026-03-12 16:32:27 +0800

    update config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
-  seed: "tsx prisma/seed.ts"
+  schema: "prisma/schema.prisma"
 });
